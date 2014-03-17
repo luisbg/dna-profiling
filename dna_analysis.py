fa9aadb2840fedae4d0d3c15db4363421ba5f8cb
Luis de Bethencourt <luis@debethencourt.com>
2014-03-17 16:57:00 -0400

    Accept multiple genome files

--- dna_analysis.py
+++ dna_analysis.py
@@ -10,24 +10,31 @@ __email__ = "luis@debethencourt.com"
 
 
 from optparse import OptionParser
-from sys import argv, exit
+from sys import argv, exit, stdout
 
 _version = 0.1
 
 
-def search_terms(words_path, genome_path, options):
+def search_terms(words_path, genome_paths, options):
     '''Search for terms in a genome file.'''
 
     if options.verbose:
         print "words file: %s" % (words_path)
-        print "genome file: %s\n" % (genome_path)
+        for gp in genome_paths:
+            print "genome file: %s\n" % (gp)
+
+    # Print header of the data table (blank cell, genome file 1, genome file N)
+    stdout.write("%20s\t" % (" "))
+    for gp in genome_paths:
+        stdout.write("%s\t" % (gp))
+    stdout.write("\n")
 
     # Open the file in words_path as wf
     with open(words_path) as wf:
         # Loop over each line of the file.
         # word contains the line (which is a word)
         for word in wf:
-            word_count = 0
+            word_counts = {}
             word = word.strip('\n')[:-1]      # Strip line formatting data.
 
             if options.verbose:
@@ -40,26 +47,33 @@ def search_terms(words_path, genome_path, options):
             if "-" in word:
                 variations.append(word.replace("-", " "))
 
-            # Loops over genome file. Not optimal but it works. Fix if
-            with open(genome_path) as gf:      # performance becomes an issue.
-                for genome in gf:
-                    if genome[0] == ">":        # Only compare header lines
-                        found = False           # Look for word in header line.
+            # Loops over genome files. Not optimal but it works. Fix if
+            for gp in genome_paths:            # performance becomes an issue.
+                word_counts[gp] = 0
 
-                        genome_l = genome.lower()
-                        if word.lower() in genome_l:
-                            found = True
-                        else:
-                            for var in variations:     # If word isn't in line,
-                                if var.lower() in genome_l:      # check variations.
-                                    found = True
+                with open(gp) as gf:
+                    for genome in gf:
+                        if genome[0] == ">":    # Only compare header lines
+                            found = False       # Look for word in header line.
 
-                        if found:
-                            word_count += 1
-                            if options.verbose:
-                                print genome
+                            genome_l = genome.lower()
+                            if word.lower() in genome_l:
+                                found = True
+                            else:                      # If wors isn't in line,
+                                for var in variations:     # check variations
+                                    if var.lower() in genome_l:
+                                        found = True
 
-            print "%s, %d" % (word, word_count)
+                            if found:
+                                word_counts[gp] += 1
+                                if options.verbose:
+                                    print genome
+
+            # Print data (search term, count in gnome file 1, count in file n)
+            stdout.write("%20s\t" % (word))
+            for gp in genome_paths:
+                stdout.write("%5d\t" % (word_counts[gp]))
+            stdout.write("\n")
 
 
 if __name__ == "__main__":
@@ -71,7 +85,7 @@ usage: %%prog [--version] [--verbose]
 Command-line DNA Analysis
 
 Positional arguments:
-    search-terms [words file] [genome file]
+    search-terms [words file] [genome file 1..2..N]
 
 Examples:
   %%prog --verbose search-terms searchwords.txt JG3.txt
@@ -95,9 +109,15 @@ Examples:
         # Check the two files are passed to the command.
         if len(args) > 2:
             words_path = args[1]
-            genome_path = args[2]
+            genome_paths = []
+
+            gfs = 2
+            while gfs < len(args):
+                genome_paths.append(args[gfs])
+                gfs += 1
+
             # If all is good, run search-terms with the files.
-            search_terms(words_path, genome_path, options)
+            search_terms(words_path, genome_paths, options)
         else:
             print "missing data files"
             print "usage: search-terms [words file] [genome file]"
